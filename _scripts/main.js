1d37c5ec08ea7dc5b32f7749a9583c2905e73af3
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2019-04-17 00:17:07 -0300

    - New post: Tabs with Vanilla JS

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,2 +1,20 @@
 
-document.body.addEventListener("touchstart", function () {});
+  var links = Array.prototype.slice.call(document.querySelectorAll('[data-target]'));
+  links.forEach(function (link){
+
+    var linkTarget = link.getAttribute('data-target');
+
+    link.addEventListener('click', function(){
+      var targets = Array.prototype.slice.call(document.querySelectorAll('[data-tab]'));
+      link.classList.remove('active');
+      targets.forEach(function (target){
+        target.setAttribute('hidden', '');
+
+        if(linkTarget === target.getAttribute('data-tab')){
+          target.removeAttribute('hidden');
+          link.classList.add('active');
+        }
+
+      })
+    });    
+  });
