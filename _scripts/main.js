99e3b4a8b010b062b540fbd5cb05a07d7ae7a172
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2019-04-17 21:32:16 -0300

    - Correction in tabs function

--- _scripts/main.js
+++ _scripts/main.js
@@ -6,15 +6,17 @@
     const allTabs = document.querySelectorAll('[data-tab]')
 
     link.addEventListener('click', function(){
-      const targets = Array.prototype.slice.call(allTabs);
-      link.classList.remove('active');
+      const targets = Array.prototype.slice.call(allTabs);      
       targets.forEach(function (target){
         target.setAttribute('hidden', '');
 
         if(linkTarget === target.getAttribute('data-tab')){
           target.removeAttribute('hidden');
+          links.forEach(btn =>{
+            btn.classList.remove('active');
+          })
           link.classList.add('active');
         }
       })
-    });    
+    });
   });
