5ed0eeae71dd74fe5fd9dea2b207311873624759
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2019-04-19 19:59:38 -0300

    - Creating elements with Vanilla JS

---
+++ _posts/2019-04-19-Creating-Elements.md
@@ -0,0 +1,28 @@
+---
+title: Creating elements with Vanilla JS
+date: 2019-04-17 00:00:00 Z
+categories:
+- Javascript
+tags:
+- javascript,
+- vanilla,
+- create element
+excerpt: 'Creating html elements only using vanilla JS.'
+img: "../img/ilustrativas/vanilla-js.jpg"
+---
+
+Creating html elements only using vanilla JS.
+
+```javacript
+const btnCreateEl = document.querySelector('[data-create]');
+btnCreateEl && btnCreateEl.addEventListener('click', ()=>{
+  const newList = document.createElement('ul');
+  const content = document.querySelector('[class="post-content"]');
+  console.log(content);
+  content.appendChild(newList);
+  newList.innerHTML = '<li>teste1</li><li>teste2</li>';
+});
+```
+<div data-grid="small-spacing row">
+    <a data-btn data-create="">Create Element</a>
+</div>
--- _scripts/main.js
+++ _scripts/main.js
@@ -1,22 +1,31 @@
-  const allTargets = document.querySelectorAll('[data-target]');
-  const links = Array.prototype.slice.call(allTargets);
+const allTargets = document.querySelectorAll('[data-target]');
+const links = Array.prototype.slice.call(allTargets);
 
-  links.forEach(function (link){
-    const linkTarget = link.getAttribute('data-target')
-    const allTabs = document.querySelectorAll('[data-tab]')
+links.forEach(function (link){
+  const linkTarget = link.getAttribute('data-target')
+  const allTabs = document.querySelectorAll('[data-tab]')
 
-    link.addEventListener('click', function(){
-      const targets = Array.prototype.slice.call(allTabs);      
-      targets.forEach(function (target){
-        target.setAttribute('hidden', '');
+  link.addEventListener('click', function(){
+    const targets = Array.prototype.slice.call(allTabs);      
+    targets.forEach(function (target){
+      target.setAttribute('hidden', '');
 
-        if(linkTarget === target.getAttribute('data-tab')){
-          target.removeAttribute('hidden');
-          links.forEach(btn =>{
-            btn.classList.remove('active');
-          })
-          link.classList.add('active');
-        }
-      })
-    });
+      if(linkTarget === target.getAttribute('data-tab')){
+        target.removeAttribute('hidden');
+        links.forEach(btn =>{
+          btn.classList.remove('active');
+        })
+        link.classList.add('active');
+      }
+    })
   });
+});
+
+const btnCreateEl = document.querySelector('[data-create]');
+btnCreateEl && btnCreateEl.addEventListener('click', ()=>{
+  const newList = document.createElement('ul');
+  const content = document.querySelector('[class="post-content"]');
+  console.log(content);
+  content.appendChild(newList);
+  newList.innerHTML = '<li>teste1</li><li>teste2</li>';
+});
