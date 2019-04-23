9fbfdab1d831f17b1bfd32856d9fc3d69f92a70a
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2019-04-22 22:38:36 -0300

    - Getting data with Fetch

--- _scripts/main.js
+++ _scripts/main.js
@@ -54,3 +54,37 @@ resultSum && resultSum.addEventListener('click', ()=>{
   newDiv.innerHTML = `${result}`
 })
 
+function createNode(element){
+  return document.createElement(element); // Create the type of element you pass in the parameters
+}
+
+function append(parent, el){
+  return parent.appendChild(el); // Append the second parameter(element) to the first one
+}
+
+const ul = document.getElementById('authors'); // Get the list where we will place our authors
+const url = 'https://randomuser.me/api/?results=10' // Get 10 random users
+
+fetch(url) // Call the fetch function passing the url of the API as a parameter
+.then((resp)=> resp.json()) // Transform the data into JSON
+.then(function(){
+  // Your code for handling the data you get from the API
+  // Create and append the li's to the ul
+  let authors = data.results; // Get the results
+  return authors.map(function(author){
+    let li = createNode('li'), // Create the elements we need
+        img = createNode('img'),
+        span = createNode('span');
+    img.src = author.picture.medium; 
+    // Add the source of the image to be the src of the img element
+    span.innerHTML = `${author.name.first} ${author.name.last}`; 
+    // Make the HTML of our span to be the first and last name of our author
+    append(li, img); // Append all our elements
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(){
+  // This is where you run code if the server returns any errors
+})
+
